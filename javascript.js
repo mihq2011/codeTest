f7c23efd82033acb601fbed3f1ae5394a15bf0c4
Mauricio Heredia <mihq1990@gmail.com>
2020-02-05 17:03:14 -0800

    updated

---
+++ javascript.js
@@ -0,0 +1,41 @@
+//DO NOT USE
+
+//Desktop and Tablet
+var tabButtons = document.querySelectorAll('.tabContainer .buttonContainer button')
+var tabPanels = document.querySelectorAll('.tabContainer .tabPanel')
+
+function showPanel (pIndex) {
+    
+    tabPanels.forEach(function(node){
+        node.style.display = 'none'
+    })
+    tabPanels[pIndex].style.display = 'block'
+}
+
+showPanel(0)
+
+
+
+
+
+
+
+
+
+//Mobile
+var contentExpand = document.querySelectorAll('.accordion__button')
+var i = 0;
+while(i < contentExpand.length) {
+    contentExpand[i].onclick = function() {
+        const accordionContent = this.nextElementSibling
+        console.log(accordionContent)
+        this.classList.toggle('accordion__button--active')
+
+        if(this.classList.contains('accordion__button--active')) {
+            accordionContent.style.maxHeight = accordionContent.scrollHeight + 'px'
+        } else{
+            accordionContent.style.maxHeight = 0
+        }
+    }
+    i++
+}
